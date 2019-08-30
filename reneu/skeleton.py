b514a1a250a54721e4d4b2edd907ab4dfde202ec
jingpengw <jingpeng.wu@gmail.com>
2019-08-30 12:07:39 -0400

    the reading and writing works

--- reneu/skeleton.py
+++ reneu/skeleton.py
@@ -1,6 +1,6 @@
 from typing import Union 
 import numpy as np
-from .lib.libreneu import update_first_child_and_sibling
+from .lib.libreneu import update_first_child_and_sibling, downsample, skeleton_to_swc_string
 
 
 class Skeleton():
@@ -23,27 +23,35 @@ class Skeleton():
         6 - end point
         7 - custom
     """
-    def __init__(self, nodes: np.ndarray, parents: np.ndarray, 
+    def __init__(self, nodes: np.ndarray, attributes: np.ndarray, 
+                    force_update_child_and_sibling: bool = True):
+        assert nodes.shape[1] == 4
+        assert attributes.shape[1] == 4
+
+        # make sure that the child and sibling are consistent with the parents
+        if force_update_child_and_sibling:
+            update_first_child_and_sibling(attributes)
+
+        self.nodes = nodes 
+        self.attributes = attributes
+    
+    @classmethod
+    def from_nodes_and_parents(cls, nodes: np.ndarray, parents: np.ndarray, 
                     classes: np.ndarray=None):
         assert nodes.shape[1] == 4
         assert nodes.shape[0] == len(parents) == len(classes)
 
-        self.nodes = nodes
         node_num = nodes.shape[0] 
 
-        self.attributes = np.zeros((node_num, 4), dtype=np.float32) 
+        attributes = np.zeros((node_num, 4), dtype=np.float32) 
         # the parents, first child and siblings should be missing initially. 
         # The zero will all point to the first node.
-        self.attributes[:, 1:] = -1
+        attributes[:, 1:] = -1
         if classes is not None:
-            self.attributes[:, 0] = classes
-        self.attributes[:, 1] = parents
-        self._update_child_and_sibling() 
-
-    def _update_child_and_sibling(self):
-        # remove current child and sibling
-        self.attributes[:, 2:4] = -1 
-        update_first_child_and_sibling(self.attributes)
+            attributes[:, 0] = classes
+        attributes[:, 1] = parents
+    
+        return cls(nodes, attributes, force_update_child_and_sibling=True)
 
     @classmethod
     def from_swc(cls, file_name: str, sort_id: bool = True):
@@ -60,13 +68,18 @@ class Skeleton():
             data = data[data[:, 0].argsort()]
 
         # the swc file stores x,y,z,r 
-        # we are defining node array as r,z,y,x to be consistent with numpy indexing
-        nodes = np.fliplr(data[:, 2:6]).astype(np.float)
+        nodes = data[:, 2:6].astype(np.float32)
 
         # numpy index is from zero rather than 1
         parents = data[:, 6] - 1
 
-        return cls(nodes, parents, classes=data[:, 1])
+        return cls.from_nodes_and_parents(nodes, parents, classes=data[:, 1])
+
+    def to_swc(self, file_name: str):
+        # the last argument is precision
+        swc_str = skeleton_to_swc_string(self.nodes, self.attributes, 3)
+        with open(file_name, 'w') as f:
+            f.write(swc_str)
 
     @property
     def node_num(self):
@@ -74,6 +87,30 @@ class Skeleton():
 
     def __len__(self):
         return self.nodes.shape[0] 
+    
+    def downsample(self, step: float=1000., modify_in_place: bool = False):
+        """Downsample the skeleton to node interval of step distance.
+        Normally the node coordinate unit is nm, so 1000 nm step will 
+        create a new skeleton with node distance about 1 micron. Note that 
+        we fixed the segment starting and ending nodes, so the first and 
+        last two nodes could be very close in one segment.
+
+        Parameters
+        -----------
+        step: distance between nodes after downsampling
+        modify_in_place: make modifycations in place to avoid memory copy. 
+            This will change current skeleton and make is unusable any more!
+
+        Return:
+        -------
+        ret: the downsampled skeleton.
+        """
+        # our downsample function changes the value of nodes and families
+        if not modify_in_place:
+            nodes = np.copy( self.nodes )
+
+        nodes, attributes = downsample(self.nodes, self.attributes)
+        return Skeleton(nodes, attributes)
 
 
 if __name__ == '__main__':
--- tests/test_skeleton.py
+++ tests/test_skeleton.py
@@ -4,6 +4,10 @@ from os.path import dirname
 from reneu.skeleton import Skeleton
 
 def test_skeleton():
-    sk = Skeleton.from_swc(joinpath(dirname(__file__), '../data/Nov10IR3e.CNG.swc'))
+    NEURON_NAME = 'Nov10IR3e.CNG'
+    sk = Skeleton.from_swc(joinpath(dirname(__file__), '../data/{}.swc'.format(NEURON_NAME)))
     assert len(sk) == 2277
     print('number of nodes: ', len(sk))
+
+    #sk = sk.downsample(step=1)
+    sk.to_swc('/tmp/{}.swc'.format(NEURON_NAME))
