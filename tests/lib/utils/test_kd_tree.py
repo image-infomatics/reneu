37722d407c40251f34cade72d63f1096f1772027
jingpengw <jingpeng.wu@gmail.com>
2019-10-24 10:37:10 -0400

    add tests to evaluate kdtree

--- tests/lib/utils/test_kd_tree.py
+++ tests/lib/utils/test_kd_tree.py
@@ -11,6 +11,13 @@ def get_nearest_node_indices(nodes, query_node, k=1, leaf_size=20):
     res = tree.query(query_node, k=k)
     return res[1]
 
+# def get_nearest_node_indices2(nodes, query_node, k=1, leaf_size=20):
+    # from sklearn.neighbors import KDTree as SKKDTree
+    # tree = SKKDTree(nodes, leaf_size=leaf_size)
+    # query_node = np.expand_dims(query_node, axis=0)
+    # _, ind = tree.query(query_node, k=k)
+    # return ind 
+
 def test_kd_tree():
     arr = np.asarray(range(24)).reshape(8,3)
     kdtree = XThreeDTree(arr, 2)
@@ -49,20 +56,35 @@ def test_kd_tree():
 
 def test_large_fake_array():
     print('\nlarger fake array test')
-    node_num = 10
-    leaf_size = 2
+    node_num = 100
+    leaf_size = 20
+    query_index = 20
     nodes = np.zeros((node_num, 3), dtype=np.float32)
     nodes[:, 0] = np.arange(0, node_num)
     nodes[:, 1] = np.arange(0, node_num)
     nodes[:, 2] = np.arange(0, node_num)
     kdtree = XThreeDTree(nodes, leaf_size)
-    query_node = np.asarray([node_num/2+0.1, node_num/2+0.1, node_num/2+0.1], dtype=np.float32)
+    query_node = np.asarray([query_index+0.1, query_index+0.1, query_index+0.1], 
+                                                                    dtype=np.float32)
     nearest_node_index = kdtree.find_nearest_k_node_indices(query_node, 1)[0]
     true_nearest_node_index = get_nearest_node_indices(nodes, query_node, 
                                                             k=1, leaf_size=leaf_size)
+
     print('nearest node index: ', nearest_node_index)
     print('true nearest node index: ', true_nearest_node_index)
     assert true_nearest_node_index == nearest_node_index
+
+    k = 20
+    nearest_node_indices = kdtree.find_nearest_k_node_indices(query_node, k)
+    true_nearest_node_indices = get_nearest_node_indices(nodes, query_node, 
+                                                            k=k, leaf_size=leaf_size)
+    # true_nearest_node_indices2 = get_nearest_node_indices2(nodes, query_node, 
+                                                            # k=k, leaf_size=leaf_size)
+
+    print(f'\nnearest {k} node indices: {nearest_node_indices}')
+    print(f'\ntrue nearest {k} node indices: {true_nearest_node_indices}')
+    assert len(set(nearest_node_indices).symmetric_difference(set(true_nearest_node_indices)))==0
+    # print(f'\ntrue nearest {k} node indices2: {true_nearest_node_indices2}')
 
 if __name__ == '__main__':
     test_large_fake_array()
