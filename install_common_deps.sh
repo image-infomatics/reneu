1eac61c1243f7fdb1ecbc949455dfc9a62d4a15e
Jingpeng Wu <jingpeng.wu@gmail.com>
2020-04-27 20:35:23 -0400

    GitHub action (#35)
    * fix github action
    
    * only use python 3.7
    
    * checkout repo before build
    
    * do not remove the c++ compile option since it do not exist in manylinux gcc version
    
    * add segmentation test. we need to find a place to put the test affinitymap and fragments data though

--- install_common_deps.sh
+++ install_common_deps.sh
@@ -3,3 +3,5 @@
 sudo apt update
 sudo apt install build-essential
 sudo apt install libboost-dev
+python -m pip install -U pip
+pip install -r requirements.txt
--- tests/test_segmentation.py
+++ tests/test_segmentation.py
@@ -5,21 +5,19 @@ import tifffile
 import numpy as np
 
 def test_supervoxel_dendrogram():
-    DIR = os.path.expanduser("~/seungmount/research/Jingpeng/02_pytorch/datasets/CREMI/seung/")
-    #DIR = os.path.expanduser("/tmp/")
-    with h5py.File(os.path.join(DIR, "affs.h5")) as f:
+    DIR = os.path.join(os.path.dirname(__file__), '../data/')
+    with h5py.File(os.path.join(DIR, "affs_test.h5"), "r") as f:
         affs = np.asarray(f["main"])
-    with h5py.File(os.path.join(DIR, "fragments.h5")) as f:
-        fragments = np.asarray(f["main"])
-        
+    
+    print('read fragments from watershed...')
+    with h5py.File(os.path.join(DIR, "fragments_test.h5"), "r") as f:
+        fragments = np.asarray(f["main"][:, :300, :300])
+     
+    print('construct dendrogram and merge supervoxels')
     dend = SupervoxelDendrogram(affs, fragments, 0.3)
-    
     seg = dend.segment(0.3)
-    breakpoint()
-
-    tifffile.imwrite(os.path.join(DIR, "seg.tif"), data=seg)
     
-    with h5py.File(os.path.join(DIR, "seg.h5"), "w") as f:
-        f.create_dataset("main", seg)
-
-
+    print('save results...')
+    tifffile.imwrite(os.path.join(DIR, "seg_test.tif"), data=seg)
+    with h5py.File(os.path.join(DIR, "seg_test.h5"), "w") as f:
+        f['main'] = seg
