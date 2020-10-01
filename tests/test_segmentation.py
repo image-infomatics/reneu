ffdb5ff04fcd7e6db0f212adcdf49fded15b5e2c
jingpengw <jingpeng.wu@gmail.com>
2020-10-01 10:20:28 -0400

    output some segmentation with a lot of mergers even with low=0.5

--- tests/test_segmentation.py
+++ tests/test_segmentation.py
@@ -1,23 +1,29 @@
 from reneu.lib.segmentation import XSupervoxelDendrogram as SupervoxelDendrogram
+from reneu.lib.segmentation import watershed
+
 import h5py
 import os
 import tifffile
 import numpy as np
 
-def test_supervoxel_dendrogram():
-    DIR = os.path.join(os.path.dirname(__file__), '../data/')
-    with h5py.File(os.path.join(DIR, "aff_160k.h5"), "r") as f:
-        affs = np.asarray(f["main"])
-    
-    print('read fragments from watershed...')
-    with h5py.File(os.path.join(DIR, "ffn_seg.h5"), "r") as f:
-        fragments = np.asarray(f["seg"])
-     
-    print('construct dendrogram and merge supervoxels')
-    dend = SupervoxelDendrogram(affs, fragments, 0.7)
-    seg = dend.segment(0.7)
-    
-    print('save results...')
-    tifffile.imwrite(os.path.join(DIR, "seg_test.tif"), data=seg)
-    with h5py.File(os.path.join(DIR, "seg_test.h5"), "w") as f:
-        f['main'] = seg
+# def test_supervoxel_dendrogram():
+DIR = os.path.join(os.path.dirname(__file__), '../data/')
+with h5py.File(os.path.join(DIR, "aff_160k.h5"), "r") as f:
+    affs = np.asarray(f["main"])
+
+print('watershed ...')
+seg = watershed(affs, 0.5, 0.99)
+tifffile.imwrite(os.path.join(DIR, "watershed_basins.tif"), data=seg)
+
+print('read fragments from watershed...')
+with h5py.File(os.path.join(DIR, "ffn_seg.h5"), "r") as f:
+    fragments = np.asarray(f["seg"])
+ 
+print('construct dendrogram and merge supervoxels')
+dend = SupervoxelDendrogram(affs, fragments, 0.7)
+seg = dend.segment(0.7)
+
+print('save results...')
+tifffile.imwrite(os.path.join(DIR, "seg_test.tif"), data=seg)
+with h5py.File(os.path.join(DIR, "seg_test.h5"), "w") as f:
+    f['main'] = seg
