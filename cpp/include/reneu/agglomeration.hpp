ea0d414c1ce34f9e2e929e494c8f6e2c2311555e
jingpengw <jingpeng.wu@gmail.com>
2020-10-02 11:18:21 -0400

    agglomeration kind of works

--- cpp/include/reneu/agglomeration.hpp
+++ cpp/include/reneu/agglomeration.hpp
@@ -19,7 +19,7 @@ class SupervoxelDendrogram{
 private:
 
 using SegPair = pair<segid_t, segid_t>;
-using EdgeProps = pair<uint32_t, aff_edge_t>;
+using EdgeProps = pair<aff_edge_t, aff_edge_t>;
 using Edge = pair<SegPair, EdgeProps>;
 using Dendrogram = vector<tuple<segid_t, segid_t, aff_edge_t>>;
 Dendrogram dendrogram;
@@ -38,28 +38,30 @@ auto build_region_graph(const AffinityMap &affs){
     assert(affs.shape(2)==fragments.shape(1));
     assert(affs.shape(3)==fragments.shape(2));
 
-    for(size_t z=0; z<fragments.shape(0); z++){
-        for(size_t y=0; y<fragments.shape(1); y++){
-            for(size_t x=0; x<fragments.shape(2); x++){
+    for(std::ptrdiff_t z=0; z<fragments.shape(0); z++){
+        for(std::ptrdiff_t y=0; y<fragments.shape(1); y++){
+            for(std::ptrdiff_t x=0; x<fragments.shape(2); x++){
                 
-                auto segid = fragments(z,y,x);
+                segid_t segid = fragments(z,y,x);
+                // skip background voxels
                 if(segid==0){ continue; } 
 
                 if (z>0 && fragments(z-1,y,x)>0 && segid!=fragments(z-1,y,x)){
                     auto key = minmax(segid, fragments(z-1,y,x));
-                    rg[key].first += 1;
+                    rg[key].first++;
                     rg[key].second += affs(2,z,y,x);
+                    //std::cout<< "region graph, first: "<< rg[key].first << ", second: "<< rg[key].second << std::endl; 
                 }
                 
                 if (y>0 && fragments(z,y-1,x)>0 && segid!=fragments(z,y-1,x)){
                     auto key = minmax(segid, fragments(z,y-1,x));
-                    rg[key].first += 1;
+                    rg[key].first++;
                     rg[key].second += affs(1,z,y,x);
                 }
                 
                 if (x>0 && fragments(z,y,x-1)>0 && segid!=fragments(z,y,x-1)){
                     auto key = minmax(segid, fragments(z,y,x-1));
-                    rg[key].first += 1;
+                    rg[key].first++;
                     rg[key].second += affs(0,z,y,x);
                 }
             }
@@ -81,7 +83,9 @@ auto greedy_mean_affinity_agglomeration( const AffinityMap &affs,
 
     // fibonacci heap might be more efficient
     // use std data structure to avoid denpendency for now
-    auto cmp = [](Edge left, Edge right){return left.second.second < right.second.second;};
+    auto cmp = [](Edge left, Edge right){return 
+                left.second.second  / left.second.first < 
+                right.second.second / right.second.first;};
     // TO-DO: replace with fibonacci heap
     priority_queue<Edge, vector<Edge>, decltype(cmp)> heap(cmp);
     for(auto &edge : rg){
@@ -90,30 +94,31 @@ auto greedy_mean_affinity_agglomeration( const AffinityMap &affs,
 
     // merge regions from the highest affinity edge
     while(!heap.empty()){
-        auto [segPair, edgeProps] = heap.top();
-        const auto [segid0, segid1] = segPair;
-        auto [count, weightSum] = edgeProps;
-        auto meanWeight = weightSum / count;
+        const auto [segPair, edgeProps] = heap.top();
+        // remove the edge in heap
+        heap.pop();
 
+        const auto [segid0, segid1] = segPair;
+        const aff_edge_t meanWeight = edgeProps.second / edgeProps.first;
+        //std::cout<< "mean weight: "<< meanWeight << std::endl;
         if( meanWeight < minThreshold ){
             std::cout<< "reached mean affinity lower than threshold: "<< meanWeight << std::endl;
             break;
         }
 
-        // remove the edge in heap
-        heap.pop();
         // add this edge to dendrogram 
         dendrogram.emplace_back(segid0, segid1, meanWeight);
         
         // merge segid1 to segid0 and update the weights
         // always merge to lower id of segments
-        for(const auto &neighbor_segid : segid2neighbors[segid1]){
+        for(const segid_t &neighbor_segid : segid2neighbors[segid1]){
             if(neighbor_segid != segid0){
                 // merge the affinity edge count and weight sum
                 auto old_segid_pair = minmax( segid1, neighbor_segid);
                 auto new_segid_pair = minmax( segid0, neighbor_segid);
                 rg[new_segid_pair].first += rg[old_segid_pair].first;
                 rg[new_segid_pair].second += rg[old_segid_pair].second;
+                rg.erase(old_segid_pair);
             }
         }
     }
@@ -129,6 +134,7 @@ SupervoxelDendrogram(const PyAffinityMap &affs, const PySegmentation &fragments_
 auto segment( const aff_edge_t &threshold ){
     auto index2segid = xt::unique(fragments);
     auto segNum = index2segid.size();
+    std::cout<< "number of fragments: "<< segNum << std::endl;
 
     unordered_map<segid_t, segid_t> segid2index;
     for(size_t index=0; index<segNum; index++){
@@ -149,7 +155,7 @@ auto segment( const aff_edge_t &threshold ){
             // Union the two sets that contain elements x and y. 
             // This is equivalent to link(find_set(x),find_set(y)).
             dsets.union_set(index0, index1);
-            mergeNum += 1;
+            mergeNum++;
         }
     }
 
