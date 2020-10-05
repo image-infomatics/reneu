9ffadc446c40fb9e14d3411279f1e40cff54f1f4
jingpengw <jingpeng.wu@gmail.com>
2020-10-05 19:56:29 -0400

    a reasonable parameter settings

--- tests/test_segmentation.py
+++ tests/test_segmentation.py
@@ -10,16 +10,25 @@ def test_supervoxel_dendrogram():
     DIR = os.path.join(os.path.dirname(__file__), '../data/')
     with h5py.File(os.path.join(DIR, "aff_160k.h5"), "r") as f:
         affs = np.asarray(f["main"])
-
+    
     print('watershed ...')
-    fragments = watershed(affs, 0.2, 0.9)
+    fragments = watershed(affs, 0.6, 0.9999)
     tifffile.imwrite(os.path.join(DIR, "watershed_basins.tif"), data=fragments)
 
+    print('dilate the fragments...')
+    #fill_background_with_affinity_guidance(fragments, affs)
+    #assert fragments.min() > 0 
+    tifffile.imwrite(os.path.join(DIR, "watershed_basins_filled.tif"), data=fragments)
+
     print('construct dendrogram and merge supervoxels')
-    dend = SupervoxelDendrogram(affs, fragments, 0.3)
-    seg = dend.segment(0.3)
+    threshold = 0.999
+    min_threshold = 0.0
+    size_threshold = 32 
+    dend = SupervoxelDendrogram(affs, fragments, min_threshold)
+    seg = dend.segment(threshold, size_threshold)
 
     print('save results...')
     tifffile.imwrite(os.path.join(DIR, "seg_test.tif"), data=seg)
     with h5py.File(os.path.join(DIR, "seg_test.h5"), "w") as f:
         f['main'] = seg
+    
