1cd03f2532360e7da89c9ba340f43c4b519a5c02
jingpengw <jingpeng.wu@gmail.com>
2020-10-13 20:19:47 -0400

    map of map is faster, but the segmentation is not changed

--- tests/test_region_graph.py
+++ tests/test_region_graph.py
@@ -1,41 +1,26 @@
-from reneu.lib.segmentation import XSupervoxelDendrogram as SupervoxelDendrogram
-from reneu.lib.segmentation import watershed, fill_background_with_affinity_guidance
+from reneu.lib.segmentation import RegionGraph 
+from reneu.lib.segmentation import watershed 
 
 import h5py
 import os
 import tifffile
 import numpy as np
 
-def test_supervoxel_dendrogram():
-    DIR = os.path.join(os.path.dirname(__file__), '../data/')
-    with h5py.File(os.path.join(DIR, "aff_160k.h5"), "r") as f:
-        affs = np.asarray(f["main"])
-    
-    print('watershed ...')
-    fragments = watershed(affs, 0.6, 0.9999)
-    tifffile.imwrite(os.path.join(DIR, "watershed_basins.tif"), data=fragments)
-    
-    print('conservative agglomeration.')
-    threshold = 0.999
-    min_threshold = 0.0
-    size_threshold = 16 
-    dend = SupervoxelDendrogram(affs, fragments, min_threshold)
-    fragments = dend.segment(threshold, size_threshold)
+#def test_region_graph():
+DIR = os.path.join(os.path.dirname(__file__), '../data/')
+with h5py.File(os.path.join(DIR, "aff_160k.h5"), "r") as f:
+    affs = np.asarray(f["main"])
 
-    print('dilate the fragments...')
-    fill_background_with_affinity_guidance(fragments, affs)
-    assert fragments.min() > 0 
-    tifffile.imwrite(os.path.join(DIR, "watershed_basins_filled.tif"), data=fragments)
+print('watershed ...')
+seg = watershed(affs, 0, 0.9999)
+tifffile.imwrite(os.path.join(DIR, "watershed_basins.tif"), data=seg)
 
-    print('more aggressive agglomeration.')
-    threshold = 0.99
-    min_threshold = 0.0
-    size_threshold = 200
-    dend = SupervoxelDendrogram(affs, fragments, min_threshold)
-    seg = dend.segment(threshold, size_threshold)
+print('construct region graph...')
+rg = RegionGraph(affs, seg) 
+print('gready mean agglomeration...')
+rg.greedy_merge_until(seg, 0.7)
 
-    print('save results...')
-    tifffile.imwrite(os.path.join(DIR, "seg_test.tif"), data=seg)
-    with h5py.File(os.path.join(DIR, "seg_test.h5"), "w") as f:
-        f['main'] = seg
-    
+print('save results...')
+tifffile.imwrite(os.path.join(DIR, "seg_rg.tif"), data=seg)
+with h5py.File(os.path.join(DIR, "seg_rg.h5"), "w") as f:
+    f['main'] = seg
