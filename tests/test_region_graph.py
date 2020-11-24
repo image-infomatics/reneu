9d6be28fb9b342fd047d634077bc8801510f4f79
jingpengw <jingpeng.wu@gmail.com>
2020-11-24 13:18:03 -0500

    works correctly

--- tests/test_region_graph.py
+++ tests/test_region_graph.py
@@ -20,32 +20,11 @@ def segment(affs: np.ndarray, seg: np.ndarray, threshold: float):
     print('region graph after segmentation: ')
     rg.print()
 
-    #print('save results...')
-    #tifffile.imwrite(os.path.join(DIR, "seg_rg.tif"), data=seg)
-    #with h5py.File(os.path.join(DIR, "seg_rg.h5"), "w") as f:
-    #    f['main'] = seg
     print("shape of segmentation: ", seg.shape)
     print(seg)
     return seg
     
     
-# #def test_region_graph():
-# #DIR = os.path.join(os.path.dirname(__file__), '../data/')
-# #with h5py.File(os.path.join(DIR, "aff_160k.h5"), "r") as f:
-# #    affs = np.asarray(f["main"])
-# affs = np.zeros((3,10,4,4), dtype=np.float32)
-# for z in range(10):
-#     affs[0,z,:,:] = z/10
-#     affs[1:,z,:,:] = (z+1) / 10
-
-
-# # print('watershed ...')
-# # seg = watershed(affs, 0, 0.9999)
-# # print(seg)
-# # tifffile.imwrite(os.path.join(DIR, "watershed_basins.tif"), data=seg)
-# seg = np.zeros((10,4,4), dtype=np.uint32)
-# for z in range(10):
-#     seg[z,:,:] = z
 
 
 def test_agglomeration():
@@ -85,6 +64,23 @@ def test_random_agglomeration():
     seg = np.arange(sz*sz, dtype=np.uint32).reshape((1,sz,sz))
     # seg += 1 
 
-    seg = segment(affs, seg, 0.1)
+    seg = segment(affs, seg, 0.2)
     print('segmentation after agglomeration: ')
     print(seg)
+
+def test_segment_large_affinity_map():
+    DIR = os.path.join(os.path.dirname(__file__), '../data/')
+    with h5py.File(os.path.join(DIR, "aff_160k.h5"), "r") as f:
+        affs = np.asarray(f["main"])
+    
+    print('watershed ...')
+    seg = watershed(affs, 0, 0.9999)
+    print(seg)
+    tifffile.imwrite(os.path.join(DIR, "watershed_basins.tif"), data=seg)
+
+    print('agglomeration...')
+    seg = segment(affs, seg, 0.3)
+    print('save results...')
+    tifffile.imwrite(os.path.join(DIR, "seg_rg.tif"), data=seg)
+    with h5py.File(os.path.join(DIR, "seg_rg.h5"), "w") as f:
+        f['main'] = seg
