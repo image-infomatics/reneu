8cbec016a0bbb1be5a8f839b278962186237687d
jingpengw <jingpeng.wu@gmail.com>
2020-11-24 23:06:15 -0500

    rename segment to agglomerate

--- tests/test_region_graph.py
+++ tests/test_region_graph.py
@@ -1,5 +1,5 @@
 from reneu.lib.segmentation import RegionGraph 
-from reneu.lib.segmentation import watershed 
+from reneu.lib.segmentation import watershed, fill_background_with_affinity_guidance 
 
 import h5py
 import os
@@ -9,19 +9,19 @@ import numpy as np
 np.random.seed(0)
 
 
-def segment(affs: np.ndarray, seg: np.ndarray, threshold: float):
+def agglomerate(affs: np.ndarray, seg: np.ndarray, threshold: float):
     print('construct region graph...')
     rg = RegionGraph(affs, seg)
 
     print('region graph before segmentation:')
-    rg.print() 
+    # rg.print() 
     print('gready mean agglomeration...')
     seg=rg.greedy_merge_until(seg, threshold)
     print('region graph after segmentation: ')
-    rg.print()
+    # rg.print()
 
     print("shape of segmentation: ", seg.shape)
-    print(seg)
+    # print(seg)
     return seg
     
     
@@ -39,7 +39,7 @@ def test_agglomeration():
     affs[1, 0, 1, 1] = 0.5
 
     print('affinity map: \n', affs)
-    seg = segment(affs, seg, 0.7)
+    seg = agglomerate(affs, seg, 0.7)
 
     print('segmentation after agglomeration: ', seg)
     
@@ -53,17 +53,22 @@ def get_random_affinity_map(sz: int):
     return affs
 
 
-def test_watershed():
+def test_watershed_and_fill_background():
     affs = get_random_affinity_map(3)
     seg = watershed(affs, 0, 0.9)
     np.testing.assert_array_equal(seg, np.array([[[1,1,1], [1,1,2], [2,2,2]]]))
+
+    ws_seg = np.copy(seg)
+    fill_background_with_affinity_guidance(seg, affs)
+    print('filled voxel number: ', np.count_nonzero(ws_seg - seg))
+   
 
 def test_random_agglomeration():
     sz = 4
     affs = get_random_affinity_map(sz)
     seg = np.arange(sz*sz, dtype=np.uint32).reshape((1,sz,sz))
 
-    seg = segment(affs, seg, 0.3)
+    seg = agglomerate(affs, seg, 0.3)
     np.testing.assert_array_equal(seg,
             np.array([[[0,  11, 11,  7], 
                        [11, 11, 11,  7],
@@ -71,19 +76,21 @@ def test_random_agglomeration():
                        [12, 11, 11, 11]]])
     )
 
-#def test_segment_large_affinity_map():
-#    DIR = os.path.join(os.path.dirname(__file__), '../data/')
-#    with h5py.File(os.path.join(DIR, "aff_160k.h5"), "r") as f:
-#        affs = np.asarray(f["main"])
-#    
-#    print('watershed ...')
-#    seg = watershed(affs, 0, 0.9999)
-#    print(seg)
-#    tifffile.imwrite(os.path.join(DIR, "watershed_basins.tif"), data=seg)
-#
-#    print('agglomeration...')
-#    seg = segment(affs, seg, 0.5)
-#    print('save results...')
-#    tifffile.imwrite(os.path.join(DIR, "seg_rg.tif"), data=seg)
-#    with h5py.File(os.path.join(DIR, "seg_rg.h5"), "w") as f:
-#        f['main'] = seg
+def test_segment_large_affinity_map():
+    DIR = os.path.join(os.path.dirname(__file__), '../data/')
+    with h5py.File(os.path.join(DIR, "aff_160k.h5"), "r") as f:
+        affs = np.asarray(f["main"])
+    
+    print('watershed ...')
+    seg = watershed(affs, 0, 0.9999)
+    tifffile.imwrite(os.path.join(DIR, "watershed_basins.tif"), data=seg)
+    # ws_seg = np.copy(seg)
+    # fill_background_with_affinity_guidance(seg, affs)
+    # print('filled voxel number: ', np.count_nonzero(ws_seg - seg))
+
+    print('agglomeration...')
+    seg = agglomerate(affs, seg, 0.5)
+    print('save results...')
+    tifffile.imwrite(os.path.join(DIR, "seg_rg.tif"), data=seg)
+    with h5py.File(os.path.join(DIR, "seg_rg.h5"), "w") as f:
+        f['main'] = seg
