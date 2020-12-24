b8ea601e541485f81a190cde8c577defa6ccc260
jingpengw <jingpeng.wu@gmail.com>
2020-12-24 05:52:08 -0500

    explicitly consider different face boundaries, but the test still failed

--- test.sh
+++ test.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-# rm -rf ./build
+rm -rf ./build
 mv python/reneu/lib/segmentation.so /tmp/
 
 python setup.py develop
 
 pytest tests/test_dendrogram.py -s
 pytest tests/test_region_graph.py -s
+pytest tests/test_region_graph_chunk.py -s
 
