9d70f975b18eb290da4af105a86931be14340a9b
jingpengw <jingpeng.wu@gmail.com>
2020-12-24 22:59:06 -0500

    fixed a memory overflow bug. the last index is shape-1.

--- cpp/include/reneu/segmentation/utils.hpp
+++ cpp/include/reneu/segmentation/utils.hpp
@@ -8,21 +8,22 @@
 
 namespace reneu{
 
-inline auto get_id2count(const Segmentation& seg){
-    std::map<segid_t, size_t> id2count = {};
-
-    for(const auto& segid : seg){
-        id2count[segid]++;
-    }
-    return id2count;
-}
-
 inline auto get_nonzero_segids(const Segmentation& seg){
-    auto id2count = get_id2count(seg);
-    std::vector<segid_t> segids;
+
+    std::map<segid_t, std::size_t> id2count;
+
+    std::cout<< "accumulate segment ids..." << std::endl;
+    for(const auto& segid : seg){
+        std::cout<< segid << ", ";
+        if(segid>0)
+            ++id2count[segid];
+    }
+
+    std::cout<< "size of map: "<< id2count.size() << std::endl;
+    std::vector<segid_t> segids = {};
     segids.reserve(id2count.size());
     for(const auto& [segid, count] : id2count){
-        if(segid>0) segids.push_back(segid);
+        segids.push_back(segid);
     }
     return segids;
 }
