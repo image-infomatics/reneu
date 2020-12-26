f4b83e3123ad908170414dbb4cb4d178aab8c3f1
jingpengw <jingpeng.wu@gmail.com>
2020-12-25 22:26:21 -0500

    print a little bit more info for debuging

--- cpp/include/reneu/segmentation/utils.hpp
+++ cpp/include/reneu/segmentation/utils.hpp
@@ -12,14 +12,14 @@ inline auto get_nonzero_segids(const Segmentation& seg){
 
     std::map<segid_t, std::size_t> id2count;
 
-    std::cout<< "accumulate segment ids..." << std::endl;
+    // std::cout<< "accumulate segment ids..." << std::endl;
     for(const auto& segid : seg){
-        std::cout<< segid << ", ";
+        // std::cout<< segid << ", ";
         if(segid>0)
             ++id2count[segid];
     }
 
-    std::cout<< "size of map: "<< id2count.size() << std::endl;
+    // std::cout<< "size of map: "<< id2count.size() << std::endl;
     std::vector<segid_t> segids = {};
     segids.reserve(id2count.size());
     for(const auto& [segid, count] : id2count){
