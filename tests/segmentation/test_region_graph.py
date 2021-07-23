a589ea93acbedaa914ce433fe49201d5916eba2c
Jingpeng Wu <jingpeng.wu@gmail.com>
2021-07-23 11:09:16 -0400

    Disjoint set array (#72)
    * update test_region_graph
    
    * merge array for disjoint sets; also add unit tests for disjoint sets
    
    * add to_array function for disjoint sets
    
    * use column as segid list, this is more consistent with other formats in machine learning

--- tests/segmentation/test_region_graph.py
+++ tests/segmentation/test_region_graph.py
@@ -51,7 +51,7 @@ def test_agglomeration():
     print('segmentation after agglomeration: ', seg)
     
     np.testing.assert_array_equal(seg, np.array([[[2,2],[4,4]]]))
-
+    
 def random_2d_affinity_map(sz: int):
     # make sure that the random array is consistent
     np.random.seed(23)
