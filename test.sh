8255491a334d6062ff5ca8d1b7d8053b22e8c40e
jingpengw <jingpeng.wu@gmail.com>
2022-02-22 13:05:07 -0500

    inplace segmentation of watershed

--- test.sh
+++ test.sh
@@ -8,16 +8,15 @@ module load gcc/11.2.0
 module load cmake 
 
 #rm -rf ./build
-#mv python/reneu/lib/segmentation.so /tmp/
-#rm python/reneu/lib/skeleton.so 
-rm python/reneu/lib/segmentation.so 
+#rm python/reneu/lib/skeleton*.so 
+rm python/reneu/lib/segmentation*.so 
 
 #python setup.py develop
 python setup.py build --debug
 
 pytest tests/segmentation/test_preprocess.py  --pdb
-#pytest tests/segmentation/test_disjoint_sets.py -s --pdb
-#pytest tests/segmentation/test_dendrogram.py -s --pdb
-#pytest tests/segmentation/test_region_graph.py -s --pdb
+pytest tests/segmentation/test_disjoint_sets.py -s --pdb
+pytest tests/segmentation/test_dendrogram.py -s --pdb
+pytest tests/segmentation/test_region_graph.py -s --pdb
 #pytest tests/segmentation/test_region_graph_chunk.py -s --pdb
 
