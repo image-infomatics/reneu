afd383c870cca527495dd534a34162e30addda85
jingpengw <jingpeng.wu@gmail.com>
2022-05-18 20:21:16 -0400

    triger

--- install_common_deps.sh
+++ install_common_deps.sh
@@ -2,6 +2,6 @@
 
 sudo apt update
 sudo apt install build-essential
-sudo apt install libboost-dev
+sudo apt install cmake xtensor-dev xtensor-python-dev libboost-dev
 python -m pip install -U pip
 pip install -r requirements.txt
