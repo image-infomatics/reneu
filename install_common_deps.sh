858ac35510ba35c604c90359b56b469a4b57391b
jingpengw <jingpeng.wu@gmail.com>
2022-06-06 11:07:27 -0400

    update

--- install_common_deps.sh
+++ install_common_deps.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-sudo apt update
+sudo apt update -y
 sudo apt install build-essential
 sudo apt install cmake xtensor-dev xtensor-python-dev libboost-dev
 python -m pip install -U pip
